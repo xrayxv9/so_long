4668224c9106160dc52640daeea24e5bd5804955
Clement Morel <cmorel@3A6.42angouleme.fr>
2024-11-25 17:29:53 +0100

    still have to check if there is ONLY ONE exit

--- parsing/main.c
+++ parsing/main.c
@@ -6,15 +6,48 @@
 /*   By: cmorel <cmorel@42angouleme.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 16:35:38 by cmorel            #+#    #+#             */
-/*   Updated: 2024/11/25 11:59:22 by cmorel           ###   ########.fr       */
+/*   Updated: 2024/11/25 17:29:20 by cmorel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../graphics/graphics.h"
 #include "parsing.h"
+#include <fcntl.h>
+
+int	check_file(char *s)
+{
+	int		i;
+	char	*t;
+	int		j;
+
+	i = 0;
+	j = 0;
+	t = "ber";
+	while (s[i] && s[i] == '.')
+		i++;
+	while (s[i + j])
+	{
+		if (s[i + j] != t[j])
+			return (0);
+		j++;
+	}
+	return (1);
+}
+
+int	tests(char *s)
+{
+	int	fd;
+
+	fd = open(s, O_RDONLY);
+	if (fd < 0)
+	{
+		ft_printf("Error\n The map didn't open please check if the map exists.");
+		return (0);
+	}
+	return (fd);
+}
 
 int	main(int argc, char **argv)
 {
-	const char	*path;
 	int			fd;
 	int			result;
 	char		**txt;
@@ -24,13 +57,11 @@ int	main(int argc, char **argv)
 		ft_printf("Error\n One map is expected.");
 		return (0);
 	}
-	path = argv[1];
-	fd = open(path, O_RDONLY);
-	if (fd < 0)
-	{
-		ft_printf("Error\n The map didn't open please check if the map exists.");
+	if (check_file(argv[1]))
 		return (-1);
-	}
+	fd = tests(argv[1]);
+	if (fd < 0)
+		return (-1);
 	txt = malloc (1 * sizeof(char *));
 	txt[0] = NULL;
 	txt = get_text(fd, txt);
